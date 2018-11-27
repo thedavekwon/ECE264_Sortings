8378aad2469d46b2e0a897b230c10f6f69ea8b9c
Do Hyung Kwon <thedavekwon@gmail.com>
2018-11-26 21:48:44 -0500

    first second thrid sort implemented

--- providedCode.cpp
+++ providedCode.cpp
@@ -133,9 +133,6 @@ void pprint(list<Data *> &l, list<Data *>::iterator stop) {
     cout << endl;
 }
 
-void timSort(list<Data *> &l) {
-}
-
 //void insertionSort(list<Data *> &l) {
 //    auto pList = l.begin();
 //    auto head = l.end();
@@ -258,9 +255,10 @@ void merge(vector<Data *> &vec, int l, int m, int r) {
     }
 }
 
-void timSort(list<Data *> &l, int n) {
-    vector<Data *> ll;
-    ll.resize(l.size());
+vector<Data *> ll(1000001);
+
+void timSort(list<Data *> &l) {
+    int n = l.size();
     auto it = l.begin();
     for (int i = 0; i < l.size(); i++) ll[i] = (*it++);
     for (int i = 0; i < n; i += RUN) insertionSort(ll, i, min((i + RUN - 1), (n - 1)));
@@ -276,18 +274,198 @@ void timSort(list<Data *> &l, int n) {
     for (auto &i : ll) (*it++) = i;
 }
 
-inline bool cmpData(Data *tmp2,  Data *tmp1) {
-    return tmp1->val1 > tmp2->val1;
+inline bool cmpData(Data *tmp1, Data *tmp2) {
+    return tmp1->val2 < tmp2->val2;
+}
+inline bool cmpData1(Data *tmp1, Data *tmp2) {
+    return tmp1->val1 < tmp2->val1;
+}
+inline bool cmpData4(Data *tmp1, Data *tmp2) {
+    return tmp1->val4 < tmp2->val4;
 }
 
-void counting_sort(list<Data *> &l, int digit, int radix) {
+int radix = 65536;
+vector<Data *> bucket(1000001);
+vector<int> countRadix(radix, 0);
 
+void lsbRadixInt1(list<Data *> &l) {
+    auto it = l.begin();
+    const int size = l.size();
+    for (int i = 0; i < size; i++) ll[i] = (*it++);
+    long long maximum = (*max_element(ll.begin(), ll.begin() + size, cmpData1))->val1;
+    int digit = 0;
+    while ((maximum >> (digit << 4)) > 0) {
+        fill(countRadix.begin(), countRadix.end(), 0);
+        for (int i = 0; i < size; i++) {
+            countRadix[(ll[i]->val1 >> (digit << 4)) % radix]++;
+        }
+        for (int i = 1; i < radix; i++) {
+            countRadix[i] += countRadix[i - 1];
+        }
+        for (int i = size - 1; i >= 0; i--) {
+            bucket[--countRadix[(ll[i]->val1 >> (digit << 4)) % radix]] = ll[i];
+        }
+        for (int i = 0; i < size; i++) {
+            ll[i] = bucket[i];
+        }
+        digit++;
+    }
+    it = l.begin();
+    for (int i = 0; i < size; i++) (*it++) = ll[i];
 }
 
-void radix_sort(list<Data *> &l, int base) {
-    int max = (*max(l.begin(), l.end()))->;
-    int it = 0;
-    while (pow(base, it) <= )
+void lsbRadixInt(list<Data *> &l) {
+    auto it = l.begin();
+    const int size = l.size();
+    for (int i = 0; i < size; i++) ll[i] = (*it++);
+    long long maximum = (*max_element(ll.begin(), ll.begin() + size, cmpData))->val2;
+    int digit = 0;
+    while ((maximum >> (digit << 4)) > 0) {
+        fill(countRadix.begin(), countRadix.end(), 0);
+        for (int i = 0; i < size; i++) {
+            countRadix[(ll[i]->val2 >> (digit << 4)) % radix]++;
+        }
+        for (int i = 1; i < radix; i++) {
+            countRadix[i] += countRadix[i - 1];
+        }
+        for (int i = size - 1; i >= 0; i--) {
+            bucket[--countRadix[(ll[i]->val2 >> (digit << 4)) % radix]] = ll[i];
+        }
+        for (int i = 0; i < size; i++) {
+            ll[i] = bucket[i];
+        }
+        digit++;
+    }
+    it = l.begin();
+    for (int i = 0; i < size; i++) (*it++) = ll[i];
+}
+
+vector<vector<Data *>> countBucket(94, vector<Data *>(1000001));
+
+void countSort(list<Data *> &l) {
+    int count[94] = {0};
+    for (auto &d : l) {
+        countBucket[d->val3 - 33][count[d->val3 - 33]++] = d;
+    }
+    auto it = l.begin();
+    int j = 0;
+    for (auto &c : countBucket) {
+        for (int i = 0; i < count[j]; i++) {
+            (*it++) = c[i];
+        }
+        j++;
+    }
+}
+
+void insertionSortString(vector<Data *> &vec, int left, int right) {
+    for (auto jt = vec.begin() + left; jt != vec.begin() + right + 1; ++jt) {
+        rotate(upper_bound(vec.begin() + left, jt, (*jt)->val4, cmpData4), jt, next(jt));
+    }
+}
+
+vector<int> countRadixString(94, 0);
+void hybridRadixSort(list<Data *> &l) {
+    auto it = l.begin();
+    const int size = l.size();
+    for (int i = 0; i < size; i++) ll[i] = (*it++);
+    int digit = 0;
+    while (digit < 3) {
+        fill(countRadixString.begin(), countRadixString.end(), 0);
+        for (int i = 0; i < size; i++) {
+            countRadixString[ll[i]->val4[digit] - 33]++;
+        }
+        for (int i = 1; i < 94; i++) {
+            countRadixString[i] += countRadixString[i - 1];
+        }
+        for (int i = size - 1; i >= 0; i--) {
+            bucket[--countRadixString[ll[i]->val4[digit] - 33]] = ll[i];
+        }
+        for (int i = 0; i < size; i++) {
+            ll[i] = bucket[i];
+        }
+        digit++;
+    }
+    for (int i = 0; i < countRadixString.size(); i++) {
+        if (i == 0) insertionSortString(ll, 0, countRadixString[i]);
+        else insertionSortString(ll, countRadixString[i-1], countRadixString[i]);
+    }
+    it = l.begin();
+    for (int i = 0; i < size; i++) (*it++) = ll[i];
+}
+
+int size;
+Data *output1[127][1000000];
+Data *input[1100000];
+
+void stringSort2(list<Data *> &l) {
+    size = l.size();
+    int count[127] = {0};
+    list<Data *>::iterator it = l.begin();
+/*
+	while( it !=l.end())
+	{
+		int ic =(*it)->val4[2];
+		output1[ic][count[ic]]=*it;
+		count[ic]++;
+		it++;
+	}
+*/
+    int k = 0;
+    do {
+        input[k] = *it;
+        it++;
+        k++;
+    } while (it != l.end());
+/*
+	int index = 0;
+	for(int i=0; i < 127; i++)
+	{
+		for(int j = 0; j < count[i]; j++)
+		{
+			input[index]=output1[i][j];
+			index++;
+		}
+	}
+*/
+    for (int pos = 3; pos >= 0; --pos) {
+        int count1[127] = {0};
+        for (int i = 0; i < size; i++) {
+            int ic = input[i]->val4[pos];
+            output1[ic][count1[ic]] = input[i];
+            count1[ic]++;
+
+        }
+
+        int index1 = 0;
+        for (int i = 0; i < 127; i++) {
+            for (int j = 0; j < count1[i]; j++) {
+                input[index1] = output1[i][j];
+                index1++;
+            }
+        }
+    }
+
+
+    int i, j;
+    Data *key;
+    for (i = 1; i < size; i++) {
+        key = input[i];
+        j = i - 1;
+
+        while (j >= 0 && input[j]->val4 > key->val4) {
+            input[j + 1] = input[j];
+            j--;
+        }
+        input[j + 1] = key;
+    }
+
+
+    it = l.begin();
+
+    for (int i = 0; i < size; i++) {
+        *it = input[i];
+        it++;
+    }
 }
 
 void sortDataList(list<Data *> &l, int field) {
@@ -295,13 +473,18 @@ void sortDataList(list<Data *> &l, int field) {
         case 1:
             //l.sort();
             //insertionSort(l);
-            timSort(l, l.size());
+            //timSort(l);
+            lsbRadixInt1(l);
             break;
         case 2:
+            lsbRadixInt(l);
             break;
         case 3:
+            countSort(l);
             break;
         case 4:
+            hybridRadixSort(l);
+            //stringSort2(l);
             break;
         default:
             break;
