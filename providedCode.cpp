76d2c9d4528f2db7a33eb6b053f2b61093a01212
Do Hyung Kwon <thedavekwon@gmail.com>
2018-12-01 18:56:24 -0500

    insertion sort fixed

--- providedCode.cpp
+++ providedCode.cpp
@@ -133,61 +133,46 @@ void pprint(list<Data *> &l, list<Data *>::iterator stop) {
     cout << endl;
 }
 
-//void insertionSort(list<Data *> &l) {
-//    auto pList = l.begin();
-//    auto head = l.end();
-//    while (pList != l.end()) {
-//        auto current = pList;
-//        pList++;
-//        pprint(l, pList);
-//        if (head == l.end() || (*current)->val1 < (*head)->val1) {
-//            current._M_node->_M_next = head._M_node;
-//            head = current;
-//        } else {
-//            auto p = head;
-//            auto pNext = p++;
-//            while (p != l.end()) {
-//                if (pNext == l.end() || (*current)->val1 < (*pNext)->val1) {
-//                    current._M_node->_M_next = pNext._M_node;
-//                    pNext._M_node = current._M_node;
-//                    break;
-//                }
-//            }
-//            p = ++p;
-//        }
-//    }
-//}
+inline bool cmp(unsigned int target, Data *tmp1) {
+    return tmp1->val1 > target;
+}
 
-//void insert(list<Data *> &l, list<Data *>::iterator &sorted, list<Data *>::iterator &current) {
-//    if (sorted == l.end() || (*sorted)->val1 >= (*current)->val1) {
-//        current._M_node->_M_next = sorted._M_node;
-//        sorted = current;
-//    } else {
-//        auto it = sorted;
-//        auto itNext = ++sorted;
-//        sorted--;
-//        while (itNext != l.end() && (*itNext)->val1 < (*current)->val1) {
-//            it++;
-//            itNext++;
-//        }
-//        current._M_node->_M_next = it._M_node->_M_next;
-//        it._M_node->_M_next = current._M_node;
-//    }
-//}
-//
-//void insertionSort(list<Data *> &l) {
-//    auto sorted = l.end();
-//    auto current = l.begin();
-//    while (current != l.end()) {
-//        auto next = ++current;
-//        current--;
-//        insert(l, sorted, current);
-//        current = next;
-//    }
-//    l.begin() = sorted;
-//}
+inline bool cmpData(Data *tmp1, Data *tmp2) {
+    return tmp1->val2 < tmp2->val2;
+}
 
-//
+inline bool cmpData1(Data *tmp1, Data *tmp2) {
+    return tmp1->val1 < tmp2->val1;
+}
+
+inline bool cmpData4(string tmp, Data *tmp2) {
+    return tmp < tmp2->val4;
+}
+
+const int RUN = 32;
+const int radix = 65536;
+
+vector<Data *> ll(1000001);
+Data *lll[1000001];
+vector<Data *> bucket(1000001);
+vector<int> countRadix(radix, 0);
+
+void insertionSortArr(list<Data *> &l) {
+    for (auto it = next(l.begin()); it != l.end(); it++) {
+        auto t = it;
+        while ((*prev(t))->val1 > (*t)->val1 && prev(t) != l.begin()) {
+//            iter_swap(prev(t), t);
+            *t._M_node = *prev(t)._M_node;
+            t--;
+        }
+        if (prev(t) == l.begin() && (*prev(t))->val1 > (*t)->val1) {
+//            iter_swap(prev(t), t);
+            *t._M_node = *prev(t)._M_node;
+        }
+        *next(t)._M_node = *it._M_node;
+    }
+}
+
 //void insertionSort(list<Data *> &l) {
 //    auto a = l.begin();
 //    auto b = l.end();
@@ -196,97 +181,104 @@ void pprint(list<Data *> &l, list<Data *>::iterator stop) {
 //    }
 //}
 
-inline bool cmp(unsigned int target, Data *tmp1) {
-    return tmp1->val1 > target;
-}
+// stl vector
+//void insertionSortVec(list<Data *> &l) {
+//    int size = l.size();
+//    auto it = l.begin();
+//    for (int i = 0; i < size; i++) { ll[i] = (*it++); }
+//
+//    for (int i = 1; i < size; i++) {
+//        auto tmp = ll[i];
+//        int j = i - 1;
+//        while (j >= 0 && ll[j]->val1 > tmp->val1) {
+//            ll[j + 1] = ll[j];
+//            j--;
+//        }
+//        ll[j + 1] = tmp;
+//    }
+//
+//    it = l.begin();
+//    for (auto jt = ll.begin(); jt != ll.begin() + size; ++jt) { (*it++) = *jt; }
+//}
 
-void insertionSort(list<Data *> &l) {
-    vector<Data *> ll;
-    ll.reserve(l.size());
-    auto it = l.begin();
-    for (int i = 0; i < l.size(); i++) { ll[i] = (*it++); }
+// array
+//void insertionSortArr(list<Data *> &l) {
+//    int size = l.size();
+//    auto it = l.begin();
+//    for (int i = 0; i < size; i++) { lll[i] = (*it++); }
+//
+//    for (int i = 1; i < size; i++) {
+//        auto tmp = lll[i];
+//        int j = i - 1;
+//        while (j >= 0 && lll[j]->val1 > tmp->val1) {
+//            lll[j + 1] = lll[j];
+//            j--;
+//        }
+//        lll[j + 1] = tmp;
+//    }
+//
+//    it = l.begin();
+//    for (int i = 0; i < size; i++) { (*it++) = lll[i]; }
+//}
 
-    for (auto jt = ll.begin(); jt != ll.end(); ++jt) {
-        rotate(upper_bound(ll.begin(), jt, (*jt)->val1, cmp), jt, next(jt));
-    }
+//void insertionSort(vector<Data *> &vec, int left, int right) {
+//    for (auto jt = vec.begin() + left; jt != vec.begin() + right + 1; ++jt) {
+//        rotate(upper_bound(vec.begin() + left, jt, (*jt)->val1, cmp), jt, next(jt));
+//    }
+//}
+//
+//void merge(vector<Data *> &vec, int l, int m, int r) {
+//    int len1 = m - l + 1;
+//    int len2 = r - m;
+//    vector<Data *> left(vec.begin() + l, vec.begin() + m + 1);
+//    vector<Data *> right(vec.begin() + m + 1, vec.begin() + r + 1);
+//
+//    int i = 0;
+//    int j = 0;
+//    int k = l;
+//
+//    while (i < len1 && j < len2) {
+//        if (left[i]->val1 <= right[j]->val1) {
+//            vec[k] = left[i];
+//            i++;
+//        } else {
+//            vec[k] = right[j];
+//            j++;
+//        }
+//        k++;
+//    }
+//
+//    while (i < len1) {
+//        vec[k] = left[i];
+//        k++;
+//        i++;
+//    }
+//
+//    while (j < len2) {
+//        vec[k] = right[j];
+//        k++;
+//        j++;
+//    }
+//}
+//
+//
+//void timSort(list<Data *> &l) {
+//    int n = l.size();
+//    auto it = l.begin();
+//    for (int i = 0; i < l.size(); i++) ll[i] = (*it++);
+//    for (int i = 0; i < n; i += RUN) insertionSort(ll, i, min((i + RUN - 1), (n - 1)));
+//
+//    for (int size = RUN; size < n; size = 2 * size) {
+//        for (int left = 0; left < n; left += 2 * size) {
+//            int mid = min(left + size - 1, n - 1);
+//            int right = min((left + 2 * size - 1), (n - 1));
+//            merge(ll, left, mid, right);
+//        }
+//    }
+//    it = l.begin();
+//    for (auto &i : ll) (*it++) = i;
+//}
 
-    it = l.begin();
-    for (auto &i : ll) { (*it++) = i; }
-}
-
-const int RUN = 32;
-
-void insertionSort(vector<Data *> &vec, int left, int right) {
-    for (auto jt = vec.begin() + left; jt != vec.begin() + right + 1; ++jt) {
-        rotate(upper_bound(vec.begin() + left, jt, (*jt)->val1, cmp), jt, next(jt));
-    }
-}
-
-void merge(vector<Data *> &vec, int l, int m, int r) {
-    int len1 = m - l + 1;
-    int len2 = r - m;
-    vector<Data *> left(vec.begin() + l, vec.begin() + m + 1);
-    vector<Data *> right(vec.begin() + m + 1, vec.begin() + r + 1);
-
-    int i = 0;
-    int j = 0;
-    int k = l;
-
-    while (i < len1 && j < len2) {
-        if (left[i]->val1 <= right[j]->val1) {
-            vec[k] = left[i];
-            i++;
-        } else {
-            vec[k] = right[j];
-            j++;
-        }
-        k++;
-    }
-    while (i < len1) {
-        vec[k] = left[i];
-        k++;
-        i++;
-    }
-
-    while (j < len2) {
-        vec[k] = right[j];
-        k++;
-        j++;
-    }
-}
-
-vector<Data *> ll(1000001);
-
-void timSort(list<Data *> &l) {
-    int n = l.size();
-    auto it = l.begin();
-    for (int i = 0; i < l.size(); i++) ll[i] = (*it++);
-    for (int i = 0; i < n; i += RUN) insertionSort(ll, i, min((i + RUN - 1), (n - 1)));
-
-    for (int size = RUN; size < n; size = 2 * size) {
-        for (int left = 0; left < n; left += 2 * size) {
-            int mid = min(left + size - 1, n - 1);
-            int right = min((left + 2 * size - 1), (n - 1));
-            merge(ll, left, mid, right);
-        }
-    }
-    it = l.begin();
-    for (auto &i : ll) (*it++) = i;
-}
-
-inline bool cmpData(Data *tmp1, Data *tmp2) {
-    return tmp1->val2 < tmp2->val2;
-}
-inline bool cmpData1(Data *tmp1, Data *tmp2) {
-    return tmp1->val1 < tmp2->val1;
-}
-inline bool cmpData4(Data *tmp1, Data *tmp2) {
-    return tmp1->val4 < tmp2->val4;
-}
-
-int radix = 65536;
-vector<Data *> bucket(1000001);
-vector<int> countRadix(radix, 0);
 
 void lsbRadixInt1(list<Data *> &l) {
     auto it = l.begin();
@@ -364,117 +356,46 @@ void insertionSortString(vector<Data *> &vec, int left, int right) {
 }
 
 vector<int> countRadixString(94, 0);
+
 void hybridRadixSort(list<Data *> &l) {
     auto it = l.begin();
     const int size = l.size();
     for (int i = 0; i < size; i++) ll[i] = (*it++);
-    int digit = 0;
-    while (digit < 3) {
+    int digit = 3;
+    while (digit >= 0) {
         fill(countRadixString.begin(), countRadixString.end(), 0);
+        // count frequencies
         for (int i = 0; i < size; i++) {
             countRadixString[ll[i]->val4[digit] - 33]++;
         }
+        // compute cumulates
         for (int i = 1; i < 94; i++) {
             countRadixString[i] += countRadixString[i - 1];
         }
-        for (int i = size - 1; i >= 0; i--) {
-            bucket[--countRadixString[ll[i]->val4[digit] - 33]] = ll[i];
+        for (int i = 0; i >= size; i--) {
+            bucket[countRadixString[ll[i]->val4[digit] - 33]++] = ll[i];
         }
         for (int i = 0; i < size; i++) {
             ll[i] = bucket[i];
         }
-        digit++;
+        digit--;
     }
     for (int i = 0; i < countRadixString.size(); i++) {
         if (i == 0) insertionSortString(ll, 0, countRadixString[i]);
-        else insertionSortString(ll, countRadixString[i-1], countRadixString[i]);
+        else insertionSortString(ll, countRadixString[i - 1], countRadixString[i]);
     }
+    insertionSortString(ll, countRadixString[countRadixString.size() - 1], size - 1);
     it = l.begin();
     for (int i = 0; i < size; i++) (*it++) = ll[i];
-}
-
-int size;
-Data *output1[127][1000000];
-Data *input[1100000];
-
-void stringSort2(list<Data *> &l) {
-    size = l.size();
-    int count[127] = {0};
-    list<Data *>::iterator it = l.begin();
-/*
-	while( it !=l.end())
-	{
-		int ic =(*it)->val4[2];
-		output1[ic][count[ic]]=*it;
-		count[ic]++;
-		it++;
-	}
-*/
-    int k = 0;
-    do {
-        input[k] = *it;
-        it++;
-        k++;
-    } while (it != l.end());
-/*
-	int index = 0;
-	for(int i=0; i < 127; i++)
-	{
-		for(int j = 0; j < count[i]; j++)
-		{
-			input[index]=output1[i][j];
-			index++;
-		}
-	}
-*/
-    for (int pos = 3; pos >= 0; --pos) {
-        int count1[127] = {0};
-        for (int i = 0; i < size; i++) {
-            int ic = input[i]->val4[pos];
-            output1[ic][count1[ic]] = input[i];
-            count1[ic]++;
-
-        }
-
-        int index1 = 0;
-        for (int i = 0; i < 127; i++) {
-            for (int j = 0; j < count1[i]; j++) {
-                input[index1] = output1[i][j];
-                index1++;
-            }
-        }
-    }
-
-
-    int i, j;
-    Data *key;
-    for (i = 1; i < size; i++) {
-        key = input[i];
-        j = i - 1;
-
-        while (j >= 0 && input[j]->val4 > key->val4) {
-            input[j + 1] = input[j];
-            j--;
-        }
-        input[j + 1] = key;
-    }
-
-
-    it = l.begin();
-
-    for (int i = 0; i < size; i++) {
-        *it = input[i];
-        it++;
-    }
 }
 
 void sortDataList(list<Data *> &l, int field) {
     switch (field) {
         case 1:
             //l.sort();
-            //insertionSort(l);
+            insertionSortArr(l);
             //timSort(l);
-            lsbRadixInt1(l);
+            //lsbRadixInt1(l);
             break;
         case 2:
             lsbRadixInt(l);
@@ -484,7 +405,6 @@ void sortDataList(list<Data *> &l, int field) {
             break;
         case 4:
             hybridRadixSort(l);
-            //stringSort2(l);
             break;
         default:
             break;
